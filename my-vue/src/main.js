7a1436cf0052f4c3859d934a8da41596f0be1ce3
leixu <leixu@quantongfu.com>
2019-01-17 18:09:41 +0800

    feat(开发--城市选择--header--搜索框):
    只完成header

--- my-vue/src/main.js
+++ my-vue/src/main.js
@@ -3,14 +3,19 @@ import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import Cube from 'cube-ui'
+import ElementUI from 'element-ui';
 import '@styleUrl/reset.css'
 import '@styleUrl/border.css'
 import '@styleUrl/iconfont.css'
 import 'swiper/dist/css/swiper.css'
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
+Vue.use(Cube)
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
@@ -19,3 +24,4 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+console.log(new Vue());
--- my-vue/src/pages/city/City.vue
+++ my-vue/src/pages/city/City.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <city-header></city-header>
+    <!--<cube-button @click="clickHandle">show dialog</cube-button>-->
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './component/Header'
+import CitySearch from './component/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
